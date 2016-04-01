ce8cb15034badc935d4cdad46b2333accd056253
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-01 12:09:03 +0300

    added location in debug area

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -17,8 +18,29 @@ class CurrentLocationViewController: UIViewController {
     @IBOutlet weak var tagButton: UIButton!
     @IBOutlet weak var getButton: UIButton!
     
+    let locatinManager = CLLocationManager()
+    
     @IBAction func getLocation(){
-        // yet nothing
+        
+        let authStatus = CLLocationManager.authorizationStatus()
+        
+        if authStatus == .Denied || authStatus == .Restricted {
+            showLocationServiceDeniedAlert()
+            return
+        }
+        
+        locatinManager.delegate = self
+        locatinManager.desiredAccuracy = kCLLocationAccuracyBest
+        locatinManager.startUpdatingLocation()
+        
+    }
+    
+    func showLocationServiceDeniedAlert(){
+        let alert = UIAlertController(title: "Location service disabled", message: "Please activate location service for this app in Settings", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        
+        alert.addAction(okAction)
+        presentViewController(alert, animated: true, completion: nil)
     }
 
     override func viewDidLoad() {
@@ -29,6 +51,16 @@ class CurrentLocationViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK : - CLLocationManagerDelegate
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Did fail with error \(error)")
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("Updated location \(newLocation)")
     }
 
 
