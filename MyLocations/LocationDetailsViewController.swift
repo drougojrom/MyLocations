5686dc0187277eeb7cd55a019898c499e6643299
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-04 14:17:11 +0300

    new segue func for detailedViewControlled and update viewDidLoad

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreLocation
 
 class LocationDetailsViewController: UITableViewController {
     
+    // MARK: outlets
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -17,11 +19,44 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
+    // MARK: var and let
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark : CLPlacemark?
+    
+    // MARK: @IBActions
     @IBAction func done(){
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func cancel(){
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK : func
+    
+    func stringFromPlacemark(placemark: CLPlacemark) -> String {
+        
+    }
+    
+    
+    // MARK: override func
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = stringFromPlacemark(placemark)
+        } else {
+            addressLabel.text = "No Address found"
+        }
+        
+        dateLabel.text = formatDate(NSDate())
     }
 }
