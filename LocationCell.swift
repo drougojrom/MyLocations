f500a72d3f126e8a90384b184afe896724823fa6
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-06 15:17:28 +0300

    added configuration for custom cell

--- LocationCell.swift
+++ LocationCell.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class LocationCell: UITableViewCell {
     
+    // MARK : outlets
+    
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
 
+    // MARK: var and let
+    
+    // MARK: func's
+    
+    func configureForLocation(location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No Description)"
+        } else {
+            descriptionLabel.text = location.locationDescription
+        }
+        
+        if let placemark = location.placemark {
+            var text = ""
+            
+            if let s = placemark.subThoroughfare {
+                text += s + " "
+            }
+            if let s = placemark.thoroughfare {
+                text += s + " "
+            }
+            if let s = placemark.locality {
+                text += s + " "
+            }
+            addressLabel.text = text
+        } else {
+            addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
+        }
+    }
+    
+    // MARK: override
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
