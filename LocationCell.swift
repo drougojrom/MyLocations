a67abef6553016e0aded8281ce15b82f4e5baa05
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-06 13:42:40 +0300

    added new file for custom UITablewView subclass

---
+++ LocationCell.swift
@@ -0,0 +1,27 @@
+//
+//  LocationCell.swift
+//  MyLocations
+//
+//  Created by Roman Ustiantcev on 06/04/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
