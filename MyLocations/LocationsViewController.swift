1caf1327a2e06e80da7c2c37555721f8802368cc
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-07 14:07:12 +0300

    implementing delegate method

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -14,50 +14,63 @@ class LocationsViewController: UITableViewController {
     
     // MARK: var and let
     var managedObjectContext: NSManagedObjectContext!
-    var locations = [Location]()
     var locationToEdit: Location?
     
+    lazy var fetchedResultsController: NSFetchedResultsController = {
+        let fetchRequest = NSFetchRequest()
+        let entity = NSEntityDescription.entityForName("Location", inManagedObjectContext: self.managedObjectContext)
+        fetchRequest.entity = entity
+        
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // fetched objects at time
+        fetchRequest.fetchBatchSize = 20
+        
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: "Locations")
+        
+        fetchedResultsController.delegate = self
+        return fetchedResultsController
+        
+    }()
+    
+    deinit {
+        fetchedResultsController.delegate = nil
+    }
+    
+    
     // MARK: func's
+    
+    func performFetch() {
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalCoreDataError(error)
+        }
+    }
     
     // MARK: override func's
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 1 - object which describes which objects you're going to fetch from the data store
-        let fetchRequest = NSFetchRequest()
-        
-        // 2 - looking for location entities
-        let entity = NSEntityDescription.entityForName("Location", inManagedObjectContext: managedObjectContext)
-        fetchRequest.entity = entity
-        
-        // 3 - sort data attributes in ascending order
-        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        
-        // 4 - returns an array with the sorted objects
-        
-        do {
-            let foundObjects = try managedObjectContext.executeFetchRequest(fetchRequest)
-            
-            // 5 - assign the context of foundObjects array to Location
-            locations = foundObjects as! [Location]
-        } catch {
-            fatalCoreDataError(error)
-        }
-        
+        performFetch()
     }
     
     // tableView
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return locations.count
+        
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.numberOfObjects
+        
+        
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
        
         let cell = tableView.dequeueReusableCellWithIdentifier("LocationCell", forIndexPath: indexPath) as! LocationCell
         
-        let location = locations[indexPath.row]
+        let location = fetchedResultsController.objectAtIndexPath(indexPath) as! Location
         cell.configureForLocation(location)
         
         return cell
@@ -71,9 +84,63 @@ class LocationsViewController: UITableViewController {
             controller.managedObjectContext = managedObjectContext
             
             if let indexPath = tableView.indexPathForCell(sender as! UITableViewCell) {
-                let location = locations[indexPath.row]
+                let location = fetchedResultsController.objectAtIndexPath(indexPath) as! Location
                 controller.locationToEdit = location
             }
         }
+    }
+}
+
+extension LocationsViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        print("controllerViewChangeContent")
+        tableView.beginUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        
+        switch type {
+        case .Insert:
+            print("NSFetchedResultsChangeInsert (object)")
+            tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+        case .Delete:
+            print("NSFetchedResultsChangeDelete (object)")
+            tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+        case .Update:
+            print("NSFetchedResultsChangeUpdate (object)")
+            if let cell = tableView.cellForRowAtIndexPath(indexPath!) {
+                let location = controller.objectAtIndexPath(indexPath!) as! Location
+                cell.configureForLocation(location)
+            }
+        case .Move:
+            print("NSFetchedResultsChangeMove (object)")
+            tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+            tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+        }
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo:NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        
+        switch type {
+            case .Insert:
+            print("NSFetchedResultsChangeInsert (section)")
+            tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+            
+            case .Delete:
+            print("NSFetchedResultsChangeDelete (section)")
+            tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+            
+            case .Update:
+            print("NSFetchedResultsChangeUpdate (section)")
+            
+            case .Move:
+            print("NSFetchedResultsChangeMove (section)")
+        }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        print("ControllerDidChangeContent")
+        tableView.endUpdates()
     }
 }
