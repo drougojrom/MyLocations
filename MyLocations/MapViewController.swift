1fed842cf32de4d63f28f3c69810cbcc1f74f4ac
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-07 16:16:58 +0300

    added MapView class file

---
+++ MyLocations/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  MyLocations
+//
+//  Created by Roman Ustiantcev on 07/04/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+    
+    
+    
+    
+}
+
+extension MapViewController : MKMapViewDelegate {
+
+
+}
